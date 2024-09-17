885715d1a2da9994ba7518bcd6a0bb73df3ff791
vincenzo-adami <51445073+vincenzo-adami@users.noreply.github.com>
2024-09-17 18:44:35 +0200

    EDIT: typo

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -2,7 +2,7 @@ package org.lessons.java;
 
 public class StampaSaluto {
 	
-	public static void main  (String args []) {
+	public static void main (String args []) {
 		System.out.println("Ciao mondo Java!");
 	}
 
